b826cc6b7acafb3cf051b4ec820fbe49e9baa088
John Speed Meyers <jsmeyers@chainguard.dev>
2022-12-03 09:18:00 -0500

    Add tests for GitHub org sponsor funding retrieval functionality
    Signed-off-by: John Speed Meyers <jsmeyers@chainguard.dev>

--- tests/utils/test_github_sponsors.py
+++ tests/utils/test_github_sponsors.py
@@ -1,32 +1,43 @@
+import json
 import unittest
 
 import funderfinder.sources.github_sponsors as gs
-
-from ..context import funderfinder
 
 
 class TestGithubSponsors(unittest.TestCase):
     def test_get_org_from_github_url(self):
         expected_org = "tensorflow"
-        self.assertEqual(expected_org, gs.get_org_from_github_url("https://github.com/tensorflow/tensorflow"))
+        self.assertEqual(
+            expected_org,
+            gs.get_org_from_github_url("https://github.com/tensorflow/tensorflow"),
+        )
+        expected_org = "tensorflow"
+        self.assertEqual(
+            "georgetown-cset",
+            gs.get_org_from_github_url(
+                "https://github.com/georgetown-cset/funder-finder"
+            ),
+        )
+
+    def test_get_gh_org_funding_json(self):
+        result = gs.get_gh_org_funding_json("georgetown-cset")
+        try:
+            json.json_loads(result)
+        except:
+            return False
 
     def test_parse_gh_org_funding_json(self):
-        pass
-        # TODO: Put in structure of JSON
-        #   example_json  =     {
-        #   "data": {
-        #     "viewer": {
-        #       "login": "jspeed-meyers"
-        #     },
-        #     "organization": {
-        #       "sponsors": {
-        #         "totalCount": 50
-        #       }
-        #     }
-        #   }
-        # }
-        #         expected_sources = ["https://www.citybureau.org/support"]
-        # expected_dict = {}
-        # self.assertEqual(
-        #     expected_dict, parse_gh_org_funding_json(example_json)
-        # )
+        example_json = """
+        {"data":
+            {"viewer":
+                {"login": "jspeed-meyers"},
+            "organization": {"sponsors": {"totalCount": 79}}
+            }
+        }
+        """
+        expected_dict = {
+            "current_sponsors_total_count": 79,
+        }
+        self.assertEqual(
+            expected_dict, gs.parse_gh_org_funding_json(json.loads(example_json))
+        )
