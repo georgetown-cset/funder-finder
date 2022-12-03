ac1d5fc83216edef05d49271c88032852ce02751
John Speed Meyers <jsmeyers@chainguard.dev>
2022-12-03 09:18:53 -0500

    Add GitHub sponsors org funding functionality
    This functionality extracts the GitHub org from a repo and assesses
    the number of sponsors for that organization. This is only a first
    draft of this GitHub functionality.
    
    Signed-off-by: John Speed Meyers <jsmeyers@chainguard.dev>

--- funderfinder/sources/github_sponsors.py
+++ funderfinder/sources/github_sponsors.py
@@ -7,8 +7,14 @@ import requests
 API_KEY = "GITHUB_TOKEN"
 
 
-def get_org_from_github_url():
-    pass
+def get_org_from_github_url(gh_url: str) -> str:
+    """
+    Returns GitHub org from a GitHub URL,
+    e.g. https://github.com/georgetown-cset/funder-finder -> georgetown-cset
+    :param gh_url: identifier for the GitHub organization
+    :return: str
+    """
+    return gh_url.split("/")[-2]
 
 
 def get_gh_org_funding_json(org: str) -> Any:
@@ -18,19 +24,21 @@ def get_gh_org_funding_json(org: str) -> Any:
     :return: JSON
     """
     query = """
-        {
+        query ($org: String!) {
           viewer {
             login
           }
-          organization(login: "curl") {
+          organization(login: $org) {
             sponsors {
-            totalCount
+              totalCount
             }
-        }
+          }
         }
     """
     variables = {"org": org}
-    headers = {"Authorization": os.environ.get(API_KEY)}
+    # adding bearer before the token is a suprising but necessary requirement
+    # see SO: https://stackoverflow.com/questions/70693292/github-graphql-api-this-endpoint-requires-you-to-be-authenticated
+    headers = {"Authorization": "bearer " + os.environ.get(API_KEY)}
     result = requests.post(
         "https://api.github.com/graphql",
         json={"query": query, "variables": variables},
@@ -46,22 +54,24 @@ def parse_gh_org_funding_json(gh_org_funding_json: Any) -> dict:
     :param org: identifier for the GitHub organization
     :return: Dict of funding stats
     """
-    # TODO: Write separate function that parses the data
-    # Need to collect the relevant data
-    # stats = data["data"]["collective"]
-    # return {
-    #     "num_contributors": stats["totalFinancialContributors"],
-    #     "amount_received_usd": stats["stats"]["totalAmountReceived"]["value"],
-    # }
+    funding_dict = {
+        "current_sponsors_total_count": gh_org_funding_json["data"]["organization"][
+            "sponsors"
+        ]["totalCount"]
+    }
+    return funding_dict
 
 
 if __name__ == "__main__":
     assert os.environ.get(API_KEY), "Please `export GITHUB_TOKEN=<your GitHub token>"
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "repo",
         help="GitHub org and repo, e.g. psf/requests",
     )
     args = parser.parse_args()
-    stats = get_gh_org_funding_json(args.repo)
+
+    org = get_org_from_github_url(args.repo)
+    stats = get_gh_org_funding_json(org)
     print(stats)
