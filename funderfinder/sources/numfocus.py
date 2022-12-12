6ffc7d14255eeaed7dc3671a4859da86a3895252
Jennifer Melot <jtmelot@gmail.com>
2022-12-12 18:19:52 -0500

    Add function to retrieve funding stats

--- funderfinder/sources/numfocus.py
+++ funderfinder/sources/numfocus.py
@@ -1,9 +1,50 @@
 import argparse
+import json
 import os
 
-import bs4
-import requests
+"""
+Get funding stats for numfocus-affiliated projects. At the moment, this only encompasses a boolean "is_affiliated"
+field that is true if the project is sponsored by or affiliated with Numfocus
+"""
+
+
+def get_funding_stats(args: dict) -> dict:
+    """
+    Determines whether a project is sponsored or affiliated with Numfocus based on our scraped dataset and
+    a project name, slug, or github owner and repo name
+    :param args: Dict of metadata that we can use to match a numfocus project
+    :return: Dict of funding stats
+    """
+    is_affiliated = False
+    with open(os.path.join("..", "data", "numfocus.jsonl")) as f:
+        for line in f:
+            meta = json.loads(line.strip())
+            for key in args:
+                is_affiliated |= (meta[key] is not None) and (meta[key] == args[key])
+    return {
+        "num_contributors": None,
+        "amount_received_usd": None,
+        "is_affiliated": is_affiliated,
+        "type": "numfocus",
+    }
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--")
+    parser.add_argument(
+        "--name", help="Case-insensitive name of the project, for example NiBabel"
+    )
+    parser.add_argument(
+        "--slug", help="Case-insensitive project slug, for example nibabel"
+    )
+    parser.add_argument(
+        "--github_name",
+        help="Case-insensitive github owner and repo name, for example nipy/nibabel",
+    )
+    args = parser.parse_args()
+
+    assert (
+        args.name or args.slug or args.github_name
+    ), "You must specify at least one of name, slug, or github_name"
+    stats = get_funding_stats(vars(args))
+    print(stats)
