a9216d90c8f53e310f66bdc2b218ea86db311e9f
Jennifer Melot <jtmelot@gmail.com>
2022-12-12 18:19:22 -0500

    Update key names, add timeout to requests

--- funderfinder/utils/list_numfocus.py
+++ funderfinder/utils/list_numfocus.py
@@ -20,9 +20,9 @@ Sponsored and Affiliated projects, but we can add this later.
 
 For now, for each project we are recording three pieces of information:
 
-  * id - this is the name of the project
+  * name - this is the name of the project
   * slug - for numfocus sponsored pages, this is the numfocus detail page slug (e.g. "nibabel" for "https://numfocus.org/project/nibabel")
-  * github_id - this is the owner/repo string of any GitHub repo we were able to associate with the project
+  * github_name - this is the owner/repo string of any GitHub repo we were able to associate with the project
   * relationship - this is "sponsored" for sponsored projects, and "affiliated" for affiliated projects
 """
 
@@ -30,6 +30,9 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:107.0) Gecko/20100101 Firefox/107.0",
     "From": "jm3312@georgetown.edu",
 }
+REQUESTS_TIMEOUT = 5
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger("list_numfocus")
 
 
 def get_github_link(text: str) -> str:
@@ -49,14 +52,21 @@ def get_sponsored_projects() -> list:
     Retrieve all numfocus sponsored projects
     :return: List of project metadata as specified in module-level documentation
     """
+    LOGGER.info("Retrieving sponsored projects")
     projects = []
-    page = requests.get("https://numfocus.org/sponsored-projects", headers=HEADERS)
+    page = requests.get(
+        "https://numfocus.org/sponsored-projects",
+        headers=HEADERS,
+        timeout=REQUESTS_TIMEOUT,
+    )
     soup = bs4.BeautifulSoup(page.text, features="html.parser")
     for project_box in soup.find_all("div", class_="search-result-item-inner"):
         link_parent = project_box.find("a", href=True)
         link = link_parent["href"]
         name = link_parent.parent.parent.text.strip()
-        project_page = requests.get(link, headers=HEADERS).text
+        project_page = requests.get(
+            link, headers=HEADERS, timeout=REQUESTS_TIMEOUT
+        ).text
         github_ref = get_github_link(project_page)
         if not github_ref:
             try:
@@ -74,16 +84,16 @@ def get_sponsored_projects() -> list:
                 if project_homepage_elt:
                     project_homepage = project_homepage_elt.parent["href"]
                     project_homepage_response = requests.get(
-                        project_homepage, headers=HEADERS
+                        project_homepage, headers=HEADERS, timeout=REQUESTS_TIMEOUT
                     )
                     github_ref = get_github_link(project_homepage_response.text)
             except Exception as e:
-                logging.warning(f"Exception when retrieving {link} for {name}: {e}")
+                LOGGER.warning(f"Exception when retrieving {link} for {name}: {e}")
         projects.append(
             {
-                "id": name,
+                "name": name,
                 "slug": link.strip().strip("/").split("/")[-1],
-                "github_id": github_ref,
+                "github_name": github_ref,
                 "relationship": "sponsored",
             }
         )
@@ -97,9 +107,12 @@ def get_affiliated_projects() -> list:
     Retrieve all numfocus affiliated projects
     :return: List of project metadata as specified in module-level documentation
     """
+    LOGGER.info("Retrieving affiliated projects")
     projects = []
     page = requests.get(
-        "https://numfocus.org/sponsored-projects/affiliated-projects", headers=HEADERS
+        "https://numfocus.org/sponsored-projects/affiliated-projects",
+        headers=HEADERS,
+        timeout=REQUESTS_TIMEOUT,
     )
     soup = bs4.BeautifulSoup(page.text, features="html.parser")
     for project_box in soup.find_all("div", class_="et_pb_blurb_content"):
@@ -113,15 +126,17 @@ def get_affiliated_projects() -> list:
         github_ref = get_github_link(link)
         if not github_ref:
             try:
-                project_page = requests.get(link, headers=HEADERS)
+                project_page = requests.get(
+                    link, headers=HEADERS, timeout=REQUESTS_TIMEOUT
+                )
                 github_ref = get_github_link(project_page.text)
             except Exception as e:
-                logging.warning(f"Exception when retrieving {link} for {name}: {e}")
+                LOGGER.warning(f"Exception when retrieving {link} for {name}: {e}")
         projects.append(
             {
-                "id": name,
+                "name": name,
                 "slug": None,
-                "github_id": github_ref,
+                "github_name": github_ref,
                 "relationship": "affiliated",
             }
         )
