05e217967c30f7a4a382a1b4b1bb8d556501bfcc
John Speed Meyers <jsmeyers@chainguard.dev>
2023-02-06 10:49:01 -0500

    Make tidelift tests consistent with codebase conventions
    Signed-off-by: John Speed Meyers <jsmeyers@chainguard.dev>

--- tests/sources/test_tidelift.py
+++ tests/sources/test_tidelift.py
@@ -2,18 +2,30 @@
 
 import unittest
 
-import funderfinder.sources.tidelift as tl
+from funderfinder.sources.tidelift import TideliftFinder
+
+from ..context import funderfinder
 
 
 class TestTidelift(unittest.TestCase):
-    def test_check_tidelift_funding_true(self):
-        self.assertEqual(
-            True,
-            tl.check_tidelift_funding("pypa/setuptools"),
+    def test_get_funding_stats_is_funded(self):
+        finder = TideliftFinder
+        stats = finder.get_funding_stats(
+            {
+                "owner": finder.get_owner_name("pypa/setuptools"),
+                "repo": finder.get_repo_name("pypa/setuptools"),
+                "is_funded": False,
+            }
         )
+        self.assertTrue(stats["is_funded"])
 
-    def test_check_tidelift_funding_false(self):
-        self.assertEqual(
-            False,
-            tl.check_tidelift_funding("georgetown-cset/funder-finder"),
+    def test_get_funding_stats_is_not_funded(self):
+        finder = TideliftFinder
+        stats = finder.get_funding_stats(
+            {
+                "owner": finder.get_owner_name("georgetown-cset/funder-finder"),
+                "repo": finder.get_repo_name("georgetown-cset/funder-finder"),
+                "is_funded": False,
+            }
         )
+        self.assertTrue(not stats["is_funded"])
