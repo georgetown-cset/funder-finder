4126c40d2d15bb9f5ca7f51c454c2c06bf94740b
Jennifer Melot <jtmelot@gmail.com>
2023-02-28 15:50:33 -0500

    Fix missing paren

--- funderfinder/utils/utils.py
+++ funderfinder/utils/utils.py
@@ -1,3 +1,3 @@
-GITHUB_PREFIX = r"github.com/([A-Za-z0-9-_.]+)"
+GITHUB_PREFIX = "github.com/([A-Za-z0-9-_.]+"
 GITHUB_REPO_PATTERN = rf"(?i){GITHUB_PREFIX}/[A-Za-z0-9-_.]*[A-Za-z0-9-_])"
-GITHUB_ORG_PATTERN = rf"(?i){GITHUB_PREFIX}(/|$)"
+GITHUB_ORG_PATTERN = rf"(?i){GITHUB_PREFIX})(/|$)"
